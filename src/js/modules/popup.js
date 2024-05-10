71b3ce02751a9f8bf1697c75bff3bec8420f1909
imurbestfriend <do764@yandex.ru>
2024-05-10 08:38:51 +0300

    popup

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -1,7 +1,14 @@
 let popup = document.querySelector('#popup');
+let closeBtn = document.querySelector('.popup-block__closeBtn').addEventListener(click, ()=>{
+    popup.style.display="none"
+})
 
 setTimeout(()=>{
  popup.style.display="block"
 }, 7000)
+
+setTimeout(()=>{
+    popup.style.display="none"
+}, 15000)
 
 // alert(55)
